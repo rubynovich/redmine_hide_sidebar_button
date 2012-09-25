eff514302a555168ea42a68595735a0967b7f26f
Rubynovich <roman@shipiev.me>
2012-09-25 16:46:12 +0400

    fix

--- lib/hsb_watchers_helper_patch.rb
+++ lib/hsb_watchers_helper_patch.rb
@@ -12,12 +12,12 @@ module HideSidebarButtonPlugin
       def watcher_tag_with_hide_button(object, user, options={})
         [watcher_tag_without_hide_button(object, user, options),
         content_tag("span", :class => :hide_sidebar) do
-          content_tag("a", :class => "icon icon-zoom-out", :href => "javascript:$('sidebar').hide();$('content').setStyle('width:auto');$$('span.show_sidebar').each(function(e){ e.show() });$$('span.hide_sidebar').each(function(e){ e.hide() });") do
+          content_tag("a", :class => "icon icon-zoom-out", :href => "#", :onclick => "javascript:$('sidebar').hide();$('content').setStyle('width:auto');$$('span.show_sidebar').each(function(e){ e.show() });$$('span.hide_sidebar').each(function(e){ e.hide() });") do
             t(:button_hide_sidebar)
           end
         end,
         content_tag("span", :class => :show_sidebar, :style => "display: none;") do
-          content_tag("a", :class => "icon icon-zoom-in", :href => "javascript:$('sidebar').show();$('content').setStyle('width:75%');$$('span.hide_sidebar').each(function(e){ e.show() });$$('span.show_sidebar').each(function(e){ e.hide() });") do
+          content_tag("a", :class => "icon icon-zoom-in", :href => "#", :onclick => "javascript:$('sidebar').show();$('content').setStyle('width:75%');$$('span.hide_sidebar').each(function(e){ e.show() });$$('span.show_sidebar').each(function(e){ e.hide() });") do
             t(:button_show_sidebar)
           end
         end].join(" ")       
