56eb03c2a8cb59c9ec00500a9c2850cc856e0181
Rubynovich <roman@shipiev.me>
2012-11-13 13:16:21 +0400

    fix перешел на русский язык в описании и README

--- init.rb
+++ init.rb
@@ -1,18 +1,29 @@
 require 'redmine'
 
-require 'dispatcher'
-require_dependency 'watchers_helper'
-require 'hsb_watchers_helper_patch'
+if Rails::VERSION::MAJOR < 3
+  require 'dispatcher'
+  object_to_prepare = Dispatcher
+else
+  object_to_prepare = Rails.configuration
+end
 
-Dispatcher.to_prepare do
-  WatchersHelper.send(:include, HideSidebarButtonPlugin::WatchersHelperPatch) unless WatchersHelper.included_modules.include? HideSidebarButtonPlugin::WatchersHelperPatch
+object_to_prepare.to_prepare do
+  [:watchers_helper].each do |cl|
+    require "hsb_#{cl}_patch"
+  end
+
+  [ 
+    [WatchersHelper, HideSidebarButtonPlugin::WatchersHelperPatch]
+  ].each do |cl, patch|
+    cl.send(:include, patch) unless cl.included_modules.include? patch
+  end
 end
 
 Redmine::Plugin.register :redmine_hide_sidebar_button do
-  name 'Redmine Hide Sidebar Button plugin'
+  name 'Кнопка "Скрыть сайдбар"'
   author 'Roman Shipiev'
-  description 'Adds a button that hides the sidebar'
-  version '0.0.1'
+  description 'Во время правки задачи и в Wiki возникают ситуации, когда сайдбар (серая область справа) мешает. Например, когда таблицы в Wiki слишком широкие. Модуль добавляет кнопку "Скрыть сайдбар", которая постредством Javascript расширяет основную область за счет удаления сайдбара. Сайдбар можно вернуть кнопкой "Показать сайдбар".'
+  version '0.0.3'
   url 'https://github.com/rubynovich/redmine_hide_sidebar_button'
   author_url 'http://roman.shipiev.me'
 end
--- lib/hsb_watchers_helper_patch.rb
+++ lib/hsb_watchers_helper_patch.rb
@@ -1,3 +1,5 @@
+require_dependency 'watchers_helper'
+
 module HideSidebarButtonPlugin
   module WatchersHelperPatch
     def self.included(base)
@@ -9,18 +11,34 @@ module HideSidebarButtonPlugin
     end
       
     module InstanceMethods
-      def watcher_tag_with_hide_button(object, user, options={})
-        [watcher_tag_without_hide_button(object, user, options),
-        content_tag("span", :class => :hide_sidebar) do
-          content_tag("a", :class => "icon icon-zoom-out", :href => "#", :onclick => "javascript:$('sidebar').hide();$('content').setStyle('width:auto');$$('span.show_sidebar').each(function(e){ e.show() });$$('span.hide_sidebar').each(function(e){ e.hide() });") do
-            t(:button_hide_sidebar)
-          end
-        end,
-        content_tag("span", :class => :show_sidebar, :style => "display: none;") do
-          content_tag("a", :class => "icon icon-zoom-in", :href => "#", :onclick => "javascript:$('sidebar').show();$('content').setStyle('width:75%');$$('span.hide_sidebar').each(function(e){ e.show() });$$('span.show_sidebar').each(function(e){ e.hide() });") do
-            t(:button_show_sidebar)
-          end
-        end].join(" ")       
+      if Rails::VERSION::MAJOR < 3
+        def watcher_tag_with_hide_button(object, user, options={})
+          [watcher_tag_without_hide_button(object, user, options),
+          content_tag("span", :class => :hide_sidebar) do
+            content_tag("a", :class => "icon icon-zoom-out", :href => "#", :onclick => "$('sidebar').hide();$('content').setStyle('width:auto');$$('span.show_sidebar').each(function(e){ e.show() });$$('span.hide_sidebar').each(function(e){ e.hide() });") do
+              t(:button_hide_sidebar)
+            end
+          end,
+          content_tag("span", :class => :show_sidebar, :style => "display: none;") do
+            content_tag("a", :class => "icon icon-zoom-in", :href => "#", :onclick => "$('sidebar').show();$('content').setStyle('width:75%');$$('span.hide_sidebar').each(function(e){ e.show() });$$('span.show_sidebar').each(function(e){ e.hide() });") do
+              t(:button_show_sidebar)
+            end
+          end].join(" ")       
+        end
+      else
+        def watcher_tag_with_hide_button(object, user, options={})
+          [watcher_tag_without_hide_button(object, user, options),
+          content_tag("span", :class => :hide_sidebar) do
+            content_tag("a", :class => "icon icon-zoom-out", :href => "#", :onclick => "$('#sidebar').hide();$('#content').css('width:auto');$('span.show_sidebar').each(function(e){ e.show() });$('span.hide_sidebar').each(function(e){ e.hide() });") do
+              t(:button_hide_sidebar)
+            end
+          end,
+          content_tag("span", :class => :show_sidebar, :style => "display: none;") do
+            content_tag("a", :class => "icon icon-zoom-in", :href => "#", :onclick => "$('#sidebar').show();$('#content').css('width:75%');$('span.hide_sidebar').each(function(e){ e.show() });$('span.show_sidebar').each(function(e){ e.hide() });") do
+              t(:button_show_sidebar)
+            end
+          end].join(" ")       
+        end      
       end
     end
   end
