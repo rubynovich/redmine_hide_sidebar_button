6c6eb2d7b2752c8633c7884577c978759dac50cf
rubynovich <roman@shipiev.me>
2013-02-14 14:36:51 +0400

    fix

--- lib/hsb_watchers_helper_patch.rb
+++ lib/hsb_watchers_helper_patch.rb
@@ -29,12 +29,12 @@ module HideSidebarButtonPlugin
         def watcher_tag_with_hide_button(object, user, options={})
           [watcher_tag_without_hide_button(object, user, options),
           content_tag("span", :class => :hide_sidebar) do
-            content_tag("a", :class => "icon icon-zoom-out", :href => "#", :onclick => "$('#sidebar').hide();$('#content').css('width:auto');$('span.show_sidebar').show();$('span.hide_sidebar').hide();") do
+            content_tag("a", :class => "icon icon-zoom-out", :href => "#", :onclick => "$('#sidebar').hide();$('#content').css({'width':'auto'});$('span.show_sidebar').show();$('span.hide_sidebar').hide();") do
               t(:button_hide_sidebar)
             end
           end,
           content_tag("span", :class => :show_sidebar, :style => "display: none;") do
-            content_tag("a", :class => "icon icon-zoom-in", :href => "#", :onclick => "$('#sidebar').show();$('#content').css('width:75%');$('span.hide_sidebar').show();$('span.show_sidebar').hide();") do
+            content_tag("a", :class => "icon icon-zoom-in", :href => "#", :onclick => "$('#sidebar').show();$('#content').css({'width':'75%'});$('span.hide_sidebar').show();$('span.show_sidebar').hide();") do
               t(:button_show_sidebar)
             end
           end].join(" ").html_safe
